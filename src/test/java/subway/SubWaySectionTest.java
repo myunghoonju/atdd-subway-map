bb37d457c18acb29ce1c95467c7edae9f7bcf9f8
MyungHoon Ju <myunghoonju@gmail.com>
2024-07-21 01:19:27 +0900

    edit test

--- src/test/java/subway/SubWaySectionTest.java
+++ src/test/java/subway/SubWaySectionTest.java
@@ -2,13 +2,16 @@ package subway;
 
 import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
+import subway.common.exception.SubWayException;
 import subway.line.domain.model.LineResponse;
 import subway.section.model.SectionRequest;
 
@@ -32,22 +35,22 @@ public class SubWaySectionTest {
     @Sql(value = {"classpath:data/reset.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @Sql(value = {"classpath:data/insert-station.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void save_section_test() {
-        LineResponse lineOld = save(1, 2, 10);
-        LineResponse lineNew = save(2, 3, 12);
+        LineResponse lineOld = addSection(1, 2, 10);
+        LineResponse lineNew = addSection(2, 3, 12);
 
         log.info("old line - {}", lineOld);
         log.info("new line - {}", lineNew);
     }
 
-    @DisplayName("지하철 1호선 마지막 역을 제거하고, 구간도 이전 구간으로 되돌리는 테스트")
+    @DisplayName("지하철 1호선 마지막 역을 제거하고(3번역), 구간도 이전 구간으로(1~2번역) 되돌리는 테스트")
     @Test
     @Sql(value = {"classpath:data/reset.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @Sql(value = {"classpath:data/insert-station.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void delete_section_test() {
         //given
         // 구간이 한개인 경우 삭제 불가하기 때문에 구역 증가
-        save(1, 2, 10);
-        save(2, 3, 12);
+        addSection(1, 2, 10);
+        addSection(2, 3, 12);
 
         //when
         // 최근 추가된 마지막 역 삭제
@@ -58,9 +61,31 @@ public class SubWaySectionTest {
         assertTrue(result);
     }
 
-    private LineResponse save(int upStation,
-                              int downStation,
-                              int distance) {
+    @DisplayName("지하철 1호선 구간이 하니인 경우 실패하는 테스트")
+    @Test
+    @Sql(value = {"classpath:data/reset.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql(value = {"classpath:data/insert-station.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    void delete_section_failure_test() {
+        //when
+        addSection(1, 1, 10);
+        //then
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value() ,delete(1));
+    }
+
+    @DisplayName("지하철 1호선 새로운 구간의 상행역이 기존 구간의 하행역 종착역이 아닌 경우 실패하는 테스트")
+    @Test
+    @Sql(value = {"classpath:data/reset.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql(value = {"classpath:data/insert-station.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    void delete_section_failure_test2() {
+        //when
+        addSection(1, 3, 10);
+        //then
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value() ,delete(2));
+    }
+
+    private LineResponse addSection(int upStation,
+                                    int downStation,
+                                    int distance) {
         return RestAssured.given()
                           .body(new SectionRequest(upStation, downStation, distance))
                           .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -68,10 +93,10 @@ public class SubWaySectionTest {
                           .post("/lines/" + 1 + "/sections").body().as(LineResponse.class);
     }
 
-    private void delete(long stationId) {
-        RestAssured.given()
-                   .param("stationId", stationId)
-                   .delete("/lines/" + 1 + "/sections");
+    private int delete(long stationId) {
+        return RestAssured.given()
+                          .param("stationId", stationId)
+                          .delete("/lines/" + 1 + "/sections").getStatusCode();
     }
 
     private LineResponse search(long id) {
